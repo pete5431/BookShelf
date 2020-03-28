386d30370348bdf3d7f3642b15a9c1f9c767a75c
pete5431 <scholarpetez@gmail.com>
2020-03-28 19:09:45 -0400

    Updated BookDetailsFragment to use new Book Class. newInstance now accepts a int along with a HashMap.

--- app/src/main/java/edu/temple/bookshelf/BookDetailsFragment.java
+++ app/src/main/java/edu/temple/bookshelf/BookDetailsFragment.java
@@ -10,18 +10,19 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.HashMap;
-import java.util.Iterator;
-
 
 public class BookDetailsFragment extends Fragment {
 
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    // the fragment initialization parameters, the HashMap and the Integer key.
     private static final String HASH_MAP_BOOK = "book_map";
+    private static final String BOOK_ID = "book_id";
 
-    private HashMap<String, String> book;
+    // Contains the name and author of the book.
+    private HashMap<Integer, Book> book;
+    // The key to get the name and author from the HashMap.
+    private int bookId;
 
-    private View v;
-
+    // The two textViews for displaying name and author.
     private TextView bookName;
     private TextView bookAuthor;
 
@@ -29,10 +30,12 @@ public class BookDetailsFragment extends Fragment {
         // Required empty public constructor
     }
 
-    public static BookDetailsFragment newInstance(HashMap<String, String> book) {
+    public static BookDetailsFragment newInstance(HashMap<Integer, Book> book, int id) {
         BookDetailsFragment fragment = new BookDetailsFragment();
         Bundle args = new Bundle();
+        // HashMap is Serializable.
         args.putSerializable(HASH_MAP_BOOK, book);
+        args.putInt(BOOK_ID, id);
         fragment.setArguments(args);
         return fragment;
     }
@@ -41,7 +44,9 @@ public class BookDetailsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            book = (HashMap<String, String>) getArguments().getSerializable(HASH_MAP_BOOK);
+            // Even though unchecked cast, HashMap is serializable.
+            book = (HashMap<Integer, Book>) getArguments().getSerializable(HASH_MAP_BOOK);
+            bookId = getArguments().getInt(BOOK_ID);
         }
     }
 
@@ -49,27 +54,36 @@ public class BookDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        v = inflater.inflate(R.layout.fragment_book_details, container, false);
+        View v = inflater.inflate(R.layout.fragment_book_details, container, false);
 
+        // Get reference to both textViews.
         bookName = v.findViewById(R.id.book_name);
         bookAuthor = v.findViewById(R.id.book_author);
 
-        if(book != null) {
-            displayBook(book);
+        // As long as the passed HashMap isn't null.
+        if(book != null){
+            // Call the method displayBook.
+            displayBook(book, bookId);
         }
+
         return v;
     }
 
-    public void displayBook(HashMap<String, String> book){
+    // Displays the book details in the textViews.
+    public void displayBook(HashMap<Integer, Book> bookMap, int position){
 
-        Iterator i = book.keySet().iterator();
+        // Get the Book Object from the HashMap.
+        Book book = bookMap.get(position);
 
-        String key = (String) i.next();
-        String value = book.get(key);
-
-        if(bookName != null && bookAuthor != null) {
-            bookName.setText(key);
-            bookAuthor.setText(value);
+        // As long as Book Object isn't null.
+        if(book != null) {
+            // Get the title or book name using the getter method of Book Class.
+            String title = book.getName();
+            // Get the author name using the getter method of Book Class.
+            String author = book.getAuthor();
+            // Set the text of the two textViews accordingly.
+            bookName.setText(title);
+            bookAuthor.setText(author);
         }
     }
 }
