549e6d763db15f3ec88a0b386a8f171d0b297dea
pete5431 <scholarpetez@gmail.com>
2020-04-02 21:21:14 -0400

    Updated Book Class to implement Parcelable, and added fields for book id and book cover image url.

--- app/src/main/java/edu/temple/bookshelf/Book.java
+++ app/src/main/java/edu/temple/bookshelf/Book.java
@@ -2,26 +2,57 @@ package edu.temple.bookshelf;
 
 // The Book Class will hold details about the actual book.
 
-public class Book {
+import android.os.Parcel;
+import android.os.Parcelable;
 
-    // The name of the book.
-    private String name;
+public class Book implements Parcelable {
+
+    // The system provided id of the book.
+    private int bookId;
+    // The title of the book.
+    private String title;
     // The author of the book.
     private String author;
+    // The url for the book cover image.
+    private String coverURL;
 
     // Accepts a name and author as constructors.
-    public Book(String name, String author){
-        this.name = name;
+    public Book(int id, String name, String author, String url){
+        this.bookId = id;
+        this.title = name;
         this.author = author;
+        this.coverURL = url;
     }
 
-    // For getting the name.
-    public String getName(){
-        return this.name;
+    protected Book(Parcel in) {
+        bookId = in.readInt();
+        title = in.readString();
+        author = in.readString();
+        coverURL = in.readString();
     }
 
-    // For getting the author.
-    public String getAuthor(){
-        return this.author;
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(bookId);
+        dest.writeString(title);
+        dest.writeString(author);
+        dest.writeString(coverURL);
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
 }
