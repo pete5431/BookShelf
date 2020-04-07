cdd84956b131e72b8c0048ad4fa907a8de95d852
pete5431 <scholarpetez@gmail.com>
2020-04-07 19:04:26 -0400

    Updated BookAdapter to use getter from Book Class to get title and author name.

--- app/src/main/java/edu/temple/bookshelf/BookAdapter.java
+++ app/src/main/java/edu/temple/bookshelf/BookAdapter.java
@@ -12,18 +12,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 // Custom Adapter BookAdapter extends ArrayAdapter that accepts HashMap Objects.
-public class BookAdapter extends ArrayAdapter<HashMap<Integer, Book>> {
+public class BookAdapter extends ArrayAdapter<Book> {
 
     // The ArrayList containing all the books.
-    private ArrayList<HashMap<Integer, Book>> books;
+    private ArrayList<Book> books;
 
     // The passed context.
     private Context context;
 
-    public BookAdapter(Context context, ArrayList<HashMap<Integer, Book>> books){
+    public BookAdapter(Context context, ArrayList<Book> books){
         super(context, 0, books);
         this.books = books;
         this.context = context;
@@ -41,26 +40,15 @@ public class BookAdapter extends ArrayAdapter<HashMap<Integer, Book>> {
             bookListItem = LayoutInflater.from(context).inflate(R.layout.list_book_item, parent, false);
         }
 
-        // Get the book HashMap from the books array.
-        HashMap<Integer, Book> bookMap = books.get(position);
-
         // Get reference to the textViews.
         TextView bookName = bookListItem.findViewById(R.id.list_book_name);
         TextView bookAuthor = bookListItem.findViewById(R.id.list_book_author);
 
-        // Get the Book object using the position as the key, which is the index of HashMap in the ArrayList.
-        Book book = bookMap.get(position);
+        Book book = books.get(position);
 
-        // If Book Object book is not null.
-        if(book != null) {
-            // Get the title and author using the getters.
-            String title = book.getName();
-            String author = book.getAuthor();
-            // Set the text accordingly.
-            bookName.setText(title);
-            bookAuthor.setText(author);
-        }
-        // Return the inflated view.
+        bookName.setText(book.getTitle());
+        bookAuthor.setText(book.getAuthor());
+
         return bookListItem;
     }
 
